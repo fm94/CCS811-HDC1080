ba210306fbd3abfb4de5a9141b58848fa139167f
fm94 <meghfares@hotmail.com>
2020-09-13 13:41:49 +0200

    modify readme

--- README.md
+++ README.md
@@ -5,7 +5,18 @@ Here I collect observations and tests, you may find some usefull code but overal
 
 v4.ino: 
 * esp8266
-* measure every 60 secs and put sensor to sleep
+* measure every 60 secs and put sensor into Sleep
 * put esp866 into periodic light sleep (50 secs), wake up before sensor and wait
 * current consumption 22mA (15 + 7) during sleep and 80mA during measurements
 * light sleep with interrupt didn't work because esp wakes up late (ccs811 gives wrong values)
+* average power consumption: 32mAh --> 768mAh/day
+
+
+v5.ino
+* esp8266
+* measure every 60 secs and put sensor into Sleep
+* put esp866 into periodic Deep sleep, wake up before sensor and wait
+* current consumption 6.3mA (0.4 + 5.9) during sleep and 80mA during measurements
+* I added the begin_deep function to the adafruit library to alow staring the sensor
+	after Deep Sleep without software reset. So please you should use the attached modified library files.
+* average power consumption: 16mAh --> 384mAh/day
--- v5/v5.ino
+++ v5/v5.ino
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+// By Fares Meghdouri
+// 13.09.2020
+// Description: A script tested with ESP8266 (NodeMCU) and the CCS811/HD1080 sensors.
+//              The script collects sensor readings and post them to a thingspeak channel.
+//              The routine includes also a Deep Sleeep mode to save power.
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Import various libraries
 #include "Adafruit_CCS811.h"
 #include "ClosedCube_HDC1080.h"
 #include <ESP8266WiFi.h>
@@ -10,34 +19,40 @@ extern "C" {
   #include "user_interface.h"
 }
 
+// Define the sensors objects
 Adafruit_CCS811 ccs;
 ClosedCube_HDC1080 hdc1080;
 
+// Define variables used throught the code
 double tmp_hdc = 0;
 double humidity = 0;
 double eco2 = 400;
 double tvoc = 0;
 
-const char *ssid = "3neo_2.4Ghz_D72F";
-const char *password = "5Y3p8HnQjC";
+// Put your wifi info here
+const char *ssid = "";
+const char *password = "";
 
-const char* apiKey = "TZ9KBKDN8AGH1AUB";
+// Put you channel write key here
+const char* apiKey = "";
 const char* resource = "/update?api_key=";
 const char* server = "api.thingspeak.com";
 
+// Measure the execution time so that the deep sleep of the sensor matches the Deep Sleep of the ESP8266
 long startTime ;
 
-// Set your Static IP address
+// I experimented with this section to use static wifi configuration but it didn't work so far
+// Set your network details
 IPAddress local_IP(192, 168, 0, 192);
-// Set your Gateway IP address
 IPAddress gateway(192, 168, 0, 1);
-
 IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(8, 8, 8, 8);   //optional
 IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
+// start the configuration (this will run after each Deep Sleep)
 void setup() {
-  
+
+  // read the reset reason
   rst_info *resetInfo;
   resetInfo = ESP.getResetInfoPtr();
   
@@ -45,29 +60,41 @@ void setup() {
   delay(10);
   Serial.println("Setup!");
 
+  // If first time staring, then configure sensors
   if ((*resetInfo).reason != 5){
       while(!ccs.begin()){
         Serial.println("Failed to start sensor! Please check your wiring.");
         delay(1000);
       }
-      
+
+      // set the period to 60 seconds
       Serial.println("set 60 secs");
       ccs.setDriveMode(CCS811_DRIVE_MODE_60SEC);
       delay(100);
-      
+
+      // since the sensor already started the 60 seconds, we need to keep track of the next wake-up
       startTime = millis();
     
       hdc1080.begin(0x40);
-    
+
+      // reduce 6 degrees from the actual reading due to the ccs811 heating up the hdc1080 (this is experimental)
       tmp_hdc = hdc1080.readTemperature() - 6;
       humidity = hdc1080.readHumidity();
+
+      // push readings (I'm not sure if we should push them after or before sleep?)
       ccs.setEnvironmentalData(tmp_hdc+6, humidity);
       delay(100);
 
+      // sleep for 60 seconds - the time needed for the configuration above. This way, we wake up
+      // imediately before the readings. Sleeping while the readings from the sensors
+      // are ready causes wrong readings. Check the tutorial on my website to understand better.
       ESP.deepSleep((60-(millis() - startTime)/1000-1)*1e6);
       
   }
   else{
+    // if we woke up from Deep Sleep then only initialise the css811 with the modified version of begin
+    // (can be found in my github repo). Initialize also the hdc1080 since the later does not cause
+    // any problems.
     while(!ccs.begin_deep()){
         Serial.println("Failed to start sensor after deep! Please check your wiring.");
         delay(1000);
@@ -77,6 +104,8 @@ void setup() {
 }
 
 
+// A function that takes the four input variables and post them to Thingspeak
+// Should be self-explanatory, you can also put yours here.
 void makeHTTPRequest(double tmp_hdc, double humidity, double eco2, double tvoc) {
 
   WiFiClient client;
@@ -116,9 +145,12 @@ void makeHTTPRequest(double tmp_hdc, double humidity, double eco2, double tvoc) 
   client.stop();
 }
 
+// A function to initialize the wifi module and Deep Sleep if it fails.
+// I tried to configure the IP statistically to speed-up the connection establishement
+// but it did not work so I droped the idea. The wifi connection with the current code takes between 2 to 5 seconds.
 void initWifi() {
 
-   Serial.print("Connecting to: "); 
+  Serial.print("Connecting to: "); 
   Serial.print(ssid);
   WiFi.begin(ssid, password);
   
@@ -126,7 +158,6 @@ void initWifi() {
   //if (!WiFi.config(local_IP, gateway, subnet)) {
    // Serial.println("STA Failed to configure");
   //}
-  
   
 
   int timeout = 50 * 4; // 20 seconds
@@ -138,6 +169,7 @@ void initWifi() {
 
   if(WiFi.status() != WL_CONNECTED) {
      Serial.println("Failed to connect wifi, going back to sleep");
+     // sleep untill the next sensor reading.
      ESP.deepSleep(45*1e6);
   }
 
@@ -149,12 +181,14 @@ void initWifi() {
 
 
 void loop() {
-  
+
+  // Woke up before the sensor thus, wait for the data to be ready.
   while(!ccs.available()){
     Serial.println("Waiting for data");
     delay(500);
     };
 
+  // measure the time needed to do the following to reduce it from the actual Deep Sleep time.
   startTime = millis();
   
   if(!ccs.readData()){ 
@@ -171,11 +205,18 @@ void loop() {
     Serial.println(eco2);
     Serial.println(tvoc);
 
+    // again not sure if I should push them before reading or after (so that they are considered for the next reading).
+    // However, this seems to give good results anyway.
     ccs.setEnvironmentalData(tmp_hdc+6, humidity);
 
     if (eco2 != 0){
+          // if the sensor is not giving 0 then send the data.
+          // when starting-up the sensor (ccs811), it gives 0 for around 4 readings.
+          
           makeHTTPRequest(tmp_hdc, humidity, eco2, tvoc);
-          Serial.println("fake sending...");
+          
+          // this is used just for debugging so that we don't spam the channel with trash.
+          //Serial.println("fake sending...");
           }
     
   }
@@ -183,9 +224,10 @@ void loop() {
   Serial.write("Sleep!");
   delay(1000);
   
-  ;
-  
+  // When everything succedes, Deep Sleep for 60 seconds - the time needed to perform all the above.
   ESP.deepSleep((60-(millis() - startTime)/1000-1)*1e6);
+
+  // This will never be executed. It was used only for debugging and checking if the Deep Sleep works.
   delay(100);
   Serial.write("Woke up!");
 }
